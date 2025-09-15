0ad51e6827b97f5df92788e3b321656c6c062dc2
jey <jeysiva.apjs@gmail.com>
2025-09-15 21:56:52 +0530

    updated repository

--- src/main/java/com/jey/skillup/repository/CourseRepository.java
+++ src/main/java/com/jey/skillup/repository/CourseRepository.java
@@ -1,7 +1,9 @@
 package com.jey.skillup.repository;
 
 import com.jey.skillup.entity.Course;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CourseRepository extends CrudRepository<Course, Long>{
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
 }
